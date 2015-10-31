f29dc4c45689206bd25d6eda705da87123bd5030
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-10-31 20:10:41 +0100

    maj

--- resources/js/OdaApp.js
+++ resources/js/OdaApp.js
@@ -20,6 +20,8 @@
      * @desc Initialize
      */
     function _init() {
+        $.Oda.Context.modeInterface = ["ajax","mokup"];
+
         $.Oda.Event.addListener({name : "oda-fully-loaded", callback : function(e){
             $.Oda.App.startApp();
         }});
@@ -37,9 +39,35 @@
          */
         startApp: function (p_params) {
             try {
+                $.Oda.Router.addMiddleWare("boniForm",function() {
+                    $.Oda.Log.debug("MiddleWares : boniForm");
+
+                    $.Oda.App.Controler.taskId = $.Oda.Router.current.args['id'];
+
+                    if($.Oda.App.Controler.taskId===0 || $.Oda.App.Controler.taskId === undefined){
+                        $.Oda.Router.routerExit = true;
+                        var msg = $.Oda.I8n.get('boniSystem','idMissingForForm');
+                        $.Oda.Log.error(msg);
+                        $.Oda.Display.Notification.error(msg);
+                    }else{
+                        var call = $.Oda.Interface.callRest($.Oda.Context.rest+"API/bpm/userTask/"+$.Oda.App.Controler.taskId+"/context", {functionRetour : function(response){
+                            $.Oda.App.Controler.context = response;
+                            var call = $.Oda.Interface.callRest($.Oda.Context.rest+"API/bpm/task/"+$.Oda.App.Controler.taskId, {functionRetour : function(response){
+                                $.Oda.App.Controler.taskInfos = response;
+                                var call = $.Oda.Interface.callRest($.Oda.Context.rest+"API/bpm/process/"+$.Oda.App.Controler.taskInfos.processId, {functionRetour : function(response){
+                                    $.Oda.App.Controler.processDefinition = response;
+                                    $.Oda.Log.info("Bonita Form ready.");
+                                    $.Oda.Event.send({name:"boniForm-ready", data : {}});
+                                }}, {});
+                            }}, {});
+                        }}, {});
+                    }
+                });
+
                 $.Oda.Router.addRoute("home", {
                     "path" : "partials/home.html",
                     "title" : "home.title",
+                    "middleWares" : ["boniForm"],
                     "urls" : ["","home"]
                 });
 
@@ -54,7 +82,7 @@
 
         Controler : {
             context : null,
-            formOutput : {"cMyVar":"coucou"},
+            formOutput : {"ctrl":"ctrl"},
             taskId : 0,
             taskInfos : null,
             processDefinition : null,
@@ -64,23 +92,90 @@
                  */
                 start: function () {
                     try {
-                        $.Oda.App.Controler.taskId = $.Oda.Router.current.args['id'];
-
-                        var call = $.Oda.Interface.callRest($.Oda.Context.rest+"API/bpm/task/"+$.Oda.App.Controler.taskId, {functionRetour : function(response){
-                            $.Oda.App.Controler.taskInfos = response;
-                            var call = $.Oda.Interface.callRest($.Oda.Context.rest+"API/bpm/process/"+$.Oda.App.Controler.taskInfos.processId, {functionRetour : function(response){
-                                $.Oda.App.Controler.processDefinition = response;
-                            }}, {});
-                        }}, {});
-
-                        var call = $.Oda.Interface.callRest($.Oda.Context.rest+"API/bpm/userTask/"+$.Oda.App.Controler.taskId+"/context", {functionRetour : function(response){
-                            $.Oda.App.Controler.context = response;
-                            $.Oda.Log.trace($.Oda.App.Controler.context);
-                        }}, {});
+                        $.Oda.Event.addListener({name : "boniForm-ready", callback : function(e){
+                            $.Oda.App.Controler.Home.startForm();
+                        }});
 
                         return this;
                     } catch (er) {
                         $.Oda.Log.error("$.Oda.App.Controler.Home.start : " + er.message);
+                        return null;
+                    }
+                },
+                /**
+                 * @returns {$.Oda.App.Controler.Home}
+                 */
+                startForm: function (p_params) {
+                    try {
+                        $.Oda.Log.trace('Hello taskId : '+$.Oda.App.Controler.taskId);
+
+                        $.Oda.Scope.Gardian.add({
+                            id : "gardianSelect3",
+                            listElt : ["select1", "select2"],
+                            function : function(params){
+                                if( ($("#select1").data("isOk")) && ($("#select2").data("isOk")) ){
+                                    $('#select3')
+                                        .find('option')
+                                        .remove()
+                                        .end()
+                                        .append('<option value="">'+ $.Oda.I8n.getByString('oda-main.select-default')+'</option>')
+                                        .append('<option value="'+$("#select1").val()+'">'+$("#select1").val()+'</option>')
+                                        .append('<option value="'+$("#select2").val()+'">'+$("#select2").val()+'</option>')
+                                        .val('')
+                                    ;
+                                }else{
+                                    $('#select3')
+                                        .find('option')
+                                        .remove()
+                                        .end()
+                                        .append('<option value="">'+ $.Oda.I8n.getByString('oda-main.select-default')+'</option>')
+                                        .val('')
+                                    ;
+                                }
+                                $.Oda.Scope.checkInputSelect({elt:$("#select3")});
+                            }
+                        });
+
+                        $.Oda.Scope.Gardian.add({
+                            id : "gardianSelect1",
+                            listElt : ["select1"],
+                            function : function(params){
+                                $('#select2')
+                                    .val('')
+                                ;
+                                $.Oda.Scope.checkInputSelect({elt:$("#select2")});
+                                $('#select3')
+                                    .find('option')
+                                    .remove()
+                                    .end()
+                                    .append('<option value="">'+ $.Oda.I8n.getByString('oda-main.select-default')+'</option>')
+                                    .val('')
+                                ;
+                                $.Oda.Scope.checkInputSelect({elt:$("#select3")});
+                            }
+                        });
+
+                        $.Oda.Scope.Gardian.add({
+                            id : "gardianSubmit",
+                            listElt : ["select1","select2","select3"],
+                            function : function(params){
+                                $.Oda.App.Controler.formOutput.select1 = $("#select1").val();
+                                $.Oda.App.Controler.formOutput.select2 = $("#select2").val();
+                                $.Oda.App.Controler.formOutput.select3 = $("#select3").val();
+                                $('#echo').html(JSON.stringify($.Oda.App.Controler.formOutput));
+                                if( ($("#select1").data("isOk")) && ($("#select2").data("isOk")) && ($("#select3").data("isOk")) ){
+                                    $("#submit").removeClass("disabled");
+                                    $("#submit").removeAttr("disabled");
+                                }else{
+                                    $("#submit").addClass("disabled");
+                                    $("#submit").attr("disabled", true);
+                                }
+                            }
+                        });
+
+                        return this;
+                    } catch (er) {
+                        $.Oda.Log.error("$.Oda.App.Controler.Home.startForm : " + er.message);
                         return null;
                     }
                 },
