4225bbd3f48f21d6f8350d4e39279f965a22b6fd
happykiller <fabrice.rosito@gmail.com>
2015-11-03 09:52:29 +0100

    update

---
+++ src/resources/config/config.js
@@ -0,0 +1,5 @@
+$.Oda.Context.host = "todefined";
+$.Oda.Context.appBonitaName = "bonita/"
+$.Oda.Context.rest = $.Oda.Context.host + $.Oda.Context.appBonitaName ;
+$.Oda.Context.projectLabel = "Empty Form";
+$.Oda.Context.resources = "";
